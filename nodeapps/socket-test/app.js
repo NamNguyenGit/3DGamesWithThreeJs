7c047f42409aa48d3ab0bc61a143daaa1cd7b1ad
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-03-04 13:09:09 +0700

    test connection

--- nodeapps/socket-test/app.js
+++ nodeapps/socket-test/app.js
@@ -1,10 +1,18 @@
-const app = require('express')();
-const http = require('http').Server(app);
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
-app.get('/', function(req,res){
-    res.sendFile(__dirname + "/index.html")
-})
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
-http.listen(3000, function(){
-    console.log("listen on port 3000")
-})
+io.on("connection", function (socket) {
+  console.log("A user connected");
+  socket.on('disconnect', function(){
+      console.log("A user disconnected");
+  })
+});
+
+http.listen(3000, function () {
+  console.log("listen on port 3000");
+});
